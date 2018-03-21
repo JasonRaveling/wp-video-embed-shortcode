27fa155876532df063c55fa263e2247d8de45ec4
Jason Raveing <jason@webunraveling.com>
2018-03-21 09:54:24 -0500

    Wrapping Vimeo with main video wrapper

--- video-sources/vimeo.php
+++ video-sources/vimeo.php
@@ -4,7 +4,7 @@ add_shortcode('vimeo', ' wunrav_vimeo_shortcode');
 function wunrav_vimeo_shortcode( $atts, $content = null ){
     extract( shortcode_atts(array("height" => '', "width" => ''), $atts) );
 
-    $vimeo = '<div style="max-width: 100%;" >';
+    $vimeo = '<div class="wunrav-video-wrapper">';
     $vimeo = $vimeo . get_vimeo($content, $width, $height, true);
     $vimeo = $vimeo . '</div>';
 
